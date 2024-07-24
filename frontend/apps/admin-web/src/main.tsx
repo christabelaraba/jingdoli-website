9995c5c73b4ab07f10df8c44eab952ebf5c3fd6a
elsieroyalty <elamegavi62@gmail.com>
2024-07-24 21:26:27 +0000

    anything

--- frontend/apps/admin-web/src/main.tsx
+++ frontend/apps/admin-web/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import AppProvider from './providers'
+import AppRouter from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+     <AppProvider>
+      <AppRouter />
+    </AppProvider>
+  </React.StrictMode>
 )
